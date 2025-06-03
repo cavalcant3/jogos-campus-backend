355842fb039f4f0bf52e60abed72bd64ed5843e0
Matheus Cavalcante <jmatheusoc269@gmail.com>
2025-06-03 09:44:03 -0300

    First Commit

---
+++ src/main/java/com/ifsergipe/jogos/JogosCampusApplication.java
@@ -0,0 +1,13 @@
+package com.ifsergipe.jogos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogosCampusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogosCampusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifsergipe/jogos/JogosCampusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsergipe.jogos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JogosCampusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
